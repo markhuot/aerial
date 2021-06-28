1f8d11f5e5884b980f5588c4111c5e819c5b06cd
Mark Huot <mark@markhuot.com>
2021-06-27 23:38:19 -0400

    adding node-fetch

--- api/videos.ts
+++ api/videos.ts
@@ -1,8 +1,9 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
+import fetch from 'node-fetch';
 
 export default function (req: VercelRequest, res: VercelResponse) {
     const { name = 'World' } = req.query
-    fetch('http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json').then(data => {
-        res.send(data)
-    })
+    fetch('http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json')
+        .then(data => data.text())
+        .then(body => res.send(body))
 }
